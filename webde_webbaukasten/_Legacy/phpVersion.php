4458bf2384933643c3393924ed42aad278360b8f
Martin Hartmond <martin.hartmond@tum.de>
2016-04-10 15:05:31 +0200

    correctly return PHP Version

--- webde_webbaukasten/_Legacy/phpVersion.php
+++ webde_webbaukasten/_Legacy/phpVersion.php
@@ -1,1 +1,12 @@
-﻿<?php phpversion() ?>
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<!--link type="text/css" rel="stylesheet" href="css/styles.css"/-->
+</head>
+<body>
+
+<h><?php echo "hello world" ?></h>
+<p>PHP Version is: <?php echo phpversion(); ?></p>
+
+</body>
+</html>
